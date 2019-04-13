910bcfcf0139b06160df12b51ee503be633b839d
octaviodegodoy <octaviodegodoy@gmail.com>
2019-04-12 22:50:30 -0300

    New client

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Link from 'react-router-dom';
+
+class Header extends Component {
+    render() {
+        return (
+            <div>
+                <Link>Redux Auth</Link>
+            </div>
+        );
+    }
+}
