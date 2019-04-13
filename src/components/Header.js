93b8c0717168f57c992e14204c795ed3fd2b9e3f
octaviodegodoy <octaviodegodoy@gmail.com>
2019-04-12 22:55:38 -0300

    New stuff

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,14 @@ class Header extends Component {
     render() {
         return (
             <div>
-                <Link>Redux Auth</Link>
+                <Link to="/">Redux Auth</Link>
+                <Link to="/signup">Sign Up</Link>
+                <Link to="/signin">Sign In</Link>
+                <Link to="/signout">Sign Out</Link>
+                <Link to="/feature">Feature</Link>
             </div>
         );
     }
 }
+
+export default Header;
