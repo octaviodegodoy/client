71836b4fcf474f68c7192cd8a3b5253a550a542f
octaviodegodoy <octaviodegodoy@gmail.com>
2019-05-23 23:03:56 -0300

    Add redux form

--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -1,5 +1,6 @@
 
 import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
 
 class Signup extends Component {
 
@@ -8,11 +9,17 @@ class Signup extends Component {
             <form>
                 <fieldset>
                     <label>Email</label>
+                    <Field
+                     name="email"
+                     type="text"
+                     component="input"
+                     />
                 </fieldset>
                 <fieldset>
                     <label>
                         Password
                     </label>
+                    <Field name="password" type="password" component="password" />
                 </fieldset>
             </form>
         );
@@ -20,4 +27,4 @@ class Signup extends Component {
 
 }
 
-export default Signup;
+export default reduxForm({ form: 'signup' })(Signup);
