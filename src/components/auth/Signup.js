3dc9116dcae7322412c6e75cc04572e5b5752a42
octaviodegodoy <octaviodegodoy@gmail.com>
2019-05-24 23:20:38 -0300

    Signup and actions

--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -1,30 +1,48 @@
 
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
 
 class Signup extends Component {
 
+    onSubmit = (formProps) => {
+        this.props.signup(formProps);
+    };
+
     render() {
+
+        const { handleSubmit } = this.props;
+
         return (
-            <form>
+            <form onSubmit={handleSubmit(this.onSubmit)} > 
                 <fieldset>
                     <label>Email</label>
                     <Field
                      name="email"
                      type="text"
                      component="input"
+                     autoComplete="none"
                      />
                 </fieldset>
                 <fieldset>
                     <label>
                         Password
                     </label>
-                    <Field name="password" type="password" component="password" />
+                    <Field name="password" type="password" component="input" autoComplete="none" />
                 </fieldset>
+                <button>Sign Up!!</button>
             </form>
         );
     }
 
 }
 
-export default reduxForm({ form: 'signup' })(Signup);
+export default compose (
+    connect(null, actions),
+    reduxForm({ form: 'signup'})
+)(Signup);
+
+
+reduxForm({ form: 'signup' })(Signup);
